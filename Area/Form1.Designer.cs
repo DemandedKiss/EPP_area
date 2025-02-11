4bc01287264b4a59f111e49992342d6a5e29353e
VikaBelova <mostordinary6@gmail.com>
2025-02-11 20:51:02 +0300

    Добавлена папка с проектом в ветку main

---
+++ Area/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Area
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxUnit = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(66, 72);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(100, 22);
+            this.textBoxLength.TabIndex = 0;
+            this.textBoxLength.TextChanged += new System.EventHandler(this.textBoxLength_TextChanged);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(66, 135);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 22);
+            this.textBoxWidth.TabIndex = 1;
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBoxWidth_TextChanged);
+            // 
+            // textBoxUnit
+            // 
+            this.textBoxUnit.Location = new System.Drawing.Point(66, 202);
+            this.textBoxUnit.Name = "textBoxUnit";
+            this.textBoxUnit.Size = new System.Drawing.Size(100, 22);
+            this.textBoxUnit.TabIndex = 2;
+            this.textBoxUnit.TextChanged += new System.EventHandler(this.textBoxUnit_TextChanged);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(103, 341);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(10, 16);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = ".";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(66, 267);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(217, 60);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Рассчитать площадь";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите длину прямоугольника:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите ширину прямоугольника:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите единицу измерения (см, м):";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ответ:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxUnit);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxLength);
+            this.Name = "Form1";
+            this.Text = "Рассчитать площадь";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxUnit;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Area/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace Area
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // Входные данные (User Input) и Механизм управления (Input Validation)
+            if (!double.TryParse(textBoxLength.Text, out double length) || length <= 0)
+            {
+                MessageBox.Show("Некорректный ввод длины. Длина должна быть положительным числом.");
+                return;
+            }
+
+            if (!double.TryParse(textBoxWidth.Text, out double width) || width <= 0)
+            {
+                MessageBox.Show("Некорректный ввод ширины. Ширина должна быть положительным числом.");
+                return;
+            }
+
+            string unit = textBoxUnit.Text.ToLower(); // Преобразуем в нижний регистр
+
+            // Механизм (Calculation) и обработка единиц измерения
+            double area = length * width;
+            string unitResult = "";
+
+            switch (unit)
+            {
+                case "м":
+                    unitResult = "м²";
+                    break;
+                case "см":
+                    unitResult = "см²";
+                    break;
+                case "мм":
+                    unitResult = "мм²";
+                    break;
+                default:
+                    MessageBox.Show("Неизвестная единица измерения. Результат выводится в м².");
+                    unitResult = "м²";
+                    break;
+            }
+
+            // Выходные данные (Output)
+            labelResult.Text = $"   {area} {unitResult}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxLength_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUnit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
